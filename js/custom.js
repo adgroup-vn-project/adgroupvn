248bd01d59c90895eec7641817729a43dda47ae2
= <=>
2019-08-21 18:08:33 +0700

    update reponsive for featured product slider

--- js/custom.js
+++ js/custom.js
@@ -15,27 +15,35 @@ $(document).ready(function () {
     });
 
     $('.js-fullheight').css('height', '400px');
-    // $('.js-logo-slider-height').css('height', '200px');
-    // $('.js-section2-height').css('height', '620px')
-    // $('.js-section-dev-height').css('height', '300px')
 
-    $('.prd-slider').owlCarousel({
-        items: 4,
-        margin: 20,
-        loop: true,
-        dots: false,
+    $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    responsiveClass:true,
+    dots: false,
         dotsEach: true,
         dotsData: true,
         autoplay: true,
         autoplayTimeout: 3000,
         autoplaySpeed: 1000,
         autoplayHoverPause: true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    }
+})
 
-
-    });
-
-    //    $('.js-fullheight').css('height', '400px');
 
 });
 
-// console.log('hello world');
